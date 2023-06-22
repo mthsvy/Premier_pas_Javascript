2ea1331cdfd6217cd1d2b69654308f1e3a14716e
Mathias <mthsvilly@gmail.com>
2023-06-22 17:49:11 +0200

    Etape 4 terminée

--- assets/script.js
+++ assets/script.js
@@ -16,39 +16,9 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-//SELECTION DE LA FLECHE GAUCHE
-const arrow_left = document.querySelector(".arrow_left");
+let counter_image = 0;
 
-//AJOUT DE LECOUTEUR EVENEMENT
-arrow_left.addEventListener("click", function() {
-	console.log("la flèche gauche a etait cliqué");
-	const dot_1 = document.querySelector(".dot_selected");
-//ENLEVER LA COULEUR BLANCHE
-dot_1.classList.remove("dot_selected");
-
-//SELECTIONNER LELEMENT PRECEDENT
-const dot_2 = dot_1.previousElementSibling;
-//AJOUTER LA COULEUR BLANCHE
-dot_2.classList.add("dot_selected");
-});
-
-//SELECTION DE LA FLECHE DROITE
-const arrow_right = document.querySelector(".arrow_right");
-
-//AJOUT DE LECOUTEUR EVENEMENT
-arrow_right.addEventListener("click", function() {
-	console.log("la flèche droite a etait cliqué");
-	//SELECTIONNER LE POINT BLANC
-const dot_1 = document.querySelector(".dot_selected");
-//ENELEVER LA COULEUR BLANCHE
-dot_1.classList.remove("dot_selected");
-
-//SELECTIONNER LELEMENT SUIVANT
-const dot_2 = dot_1.nextElementSibling;
-//AJOUTER LA COULEUR BLANCHE
-dot_2.classList.add("dot_selected");
-});
-
+//----------------------------------------------------- AJOUT DES POINTS SOUS LIMAGE ----------------------
 //SELECTION DE LEMPLACEMENT POUR AJOUTER LES POINT SOUS LIMAGE
 const dots = document.querySelector(".dots");
 
@@ -63,6 +33,94 @@ for (let i = 0; i< slides.length; i++) {
 		dot.classList.add("dot_selected");
 	}
 }
+
+//----------------------------------------------------- DEFILEMENT DES IMAGES EN FONCTION DU CLICK SUR LES FLECHES ----------------------
+//SELECTION DE LA FLECHE GAUCHE
+const arrow_left = document.querySelector(".arrow_left");
+
+//AJOUT DE LECOUTEUR D'EVENEMENT FLECHE GAUCHE
+arrow_left.addEventListener("click", function() {
+	console.log("la flèche gauche a etait cliqué");
+	const dot_1 = document.querySelector(".dot_selected");
+	
+	//ENLEVER LA COULEUR BLANCHE
+	dot_1.classList.remove("dot_selected");
+
+	//SELECTIONNER LELEMENT PRECEDENT
+	const dot_2 = dot_1.previousElementSibling;
+
+	//AJOUTER LA COULEUR BLANCHE
+	dot_2.classList.add("dot_selected");
+
+	//JE SELECTIONNE LIMAGE A MODIFIER
+	const image = document.querySelector(".banner-img");
+	
+	//RECULE DE 1 LE CAROUSSEL
+	counter_image--;
+
+	//JE MODIFIE LA SRC DE LIMAGE QUE JE VEUX MODIFIER
+	image.src = "./assets/images/slideshow/" + slides[counter_image].image;	
+	
+	//JE SELECTIONNE LE TEXTE A MODIFIER
+	const tagline = document.querySelector("#banner p");
+
+	//MODIFIER LE TEXTE
+	tagline.innerHTML = slides[counter_image].tagLine;
+});
+
+//SELECTION DE LA FLECHE DROITE
+const arrow_right = document.querySelector(".arrow_right");
+
+//AJOUT DE LECOUTEUR EVENEMENT FLECHE DROITE
+arrow_right.addEventListener("click", function() {
+	console.log("la flèche droite a etait cliqué");	
+
+	//SELECTIONNER LE POINT BLANC
+	const dot_1 = document.querySelector(".dot_selected");
+
+	//ENLEVER LA COULEUR BLANCHE
+	dot_1.classList.remove("dot_selected");
+
+	//SELECTIONNER LELEMENT SUIVANT
+	const dot_2 = dot_1.nextElementSibling;
+
+	//AJOUTER LA COULEUR BLANCHE
+	dot_2.classList.add("dot_selected");
+
+	//JE SELECTIONNE LIMAGE A MODIFIER
+	const image = document.querySelector(".banner-img");
+
+	//AVANCE DE 1 LE CAROUSSEL
+	counter_image++;
+	//JE MODIFIE LA SRC DE LIMAGE QUE JE VEUX MODIFIER
+	image.src = "./assets/images/slideshow/" + slides[counter_image].image;
+
+	//JE SELECTIONNE LE TEXTE A MODIFIER
+	const tagline = document.querySelector("#banner p");
+
+	//MODIFIER LE TEXTE
+	tagline.innerHTML = slides[counter_image].tagLine;
+});
+
+	
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+ 
+
+
+
+
 
 
 
